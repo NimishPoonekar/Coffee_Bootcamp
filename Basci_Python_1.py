61f9e170d8bec40b21c8b27f3c0acc7ccd448a69
NimishPoonekar <110768969+NimishPoonekar@users.noreply.github.com>
2022-08-07 17:41:40 +0530

    Create Basci_Python_1.py

---
+++ Basci_Python_1.py
@@ -0,0 +1,7 @@
+1. #Write a Python program which accepts the user's first and last name and prints them in reverse order with a space between them.
+
+print("Your First Name")
+f_name = input()
+print("Your Last Name")
+l_name = input()
+print(l_name, f_name)
