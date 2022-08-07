ddae63883b1d0e61abfadcbba776e3360142430e
NimishPoonekar <110768969+NimishPoonekar@users.noreply.github.com>
2022-08-07 18:06:17 +0530

    Create Basci_Python_3.py

---
+++ Basci_Python_3.py
@@ -0,0 +1,9 @@
+#Write a Python program to display the first and last colours from the following list.
+#color_list = ["Red","Green","White" ,"Black"]
+
+color_list = ["Red","Green","White" ,"Black"]
+
+color_list.remove("Green")
+color_list.remove("White")
+
+print("List: ", color_list)
