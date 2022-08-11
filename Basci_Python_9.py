fa525a8d5e854065f033c9a872b1e94bdd002658
NimishPoonekar <110768969+NimishPoonekar@users.noreply.github.com>
2022-08-11 18:44:03 +0530

    Create Basci_Python_9.py

---
+++ Basci_Python_9.py
@@ -0,0 +1,9 @@
+#Write a Python program to concatenate all elements in a list into a string and return it.
+
+def concatenate_list(list):
+    result= ''
+    for element in list:
+        result += str(element)
+    return result
+ 
+print(concatenate_list([5,2,7,4]))
